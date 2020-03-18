fe0901fa86bf791cf5b724e677ccd7caa61f6d3d
Mohamed Ismail Arif <mhdarif.vdm@gmail.com>
2020-03-18 13:10:47 +0530

    first commit - empty project

---
+++ src/test/java/co/in/javacoder/billingapp/BillingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.in.javacoder.billingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
