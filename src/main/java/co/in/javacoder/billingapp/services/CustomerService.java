09a3cbd189945d20afa288d6590706bf2632af66
Mohamed Ismail Arif <mhdarif.vdm@gmail.com>
2020-03-18 18:06:48 +0530

    Service classes are created.

---
+++ src/main/java/co/in/javacoder/billingapp/services/CustomerService.java
@@ -0,0 +1,15 @@
+package co.in.javacoder.billingapp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.in.javacoder.billingapp.entities.Customer;
+import co.in.javacoder.billingapp.repositories.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+	
+}
---
+++ src/main/java/co/in/javacoder/billingapp/services/InvoiceService.java
@@ -0,0 +1,8 @@
+package co.in.javacoder.billingapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvoiceService {
+
+}
